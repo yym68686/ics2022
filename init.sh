82c241a52f446af8c8ea4bbac72597fbf02fa15f
161530115-Jin Hang <jinhang@nuaa.edu.cn>
2019-02-28 00:27:57 +0800

    modify init.sh

--- init.sh
+++ init.sh
@@ -3,16 +3,16 @@
 log=""
 
 function init() {
-  if [ -d $1 ]; then
-    echo "$1 is already initialized, exiting..."
-    return
-  fi
+#  if [ -d $1 ]; then
+#    echo "$1 is already initialized, exiting..."
+#    return
+#  fi
 
-  while [ ! -d $1 ]; do
-    git clone -b ics2017 https://github.com/NJU-ProjectN/$1.git
-  done
-  log="$log$1 `cd $1 && git log --oneline --no-abbrev-commit -n1`"$'\n'
-  rm -rf $1/.git
+#  while [ ! -d $1 ]; do
+#    git clone -b ics2017 https://github.com/NJU-ProjectN/$1.git
+#  done
+#  log="$log$1 `cd $1 && git log --oneline --no-abbrev-commit -n1`"$'\n'
+#  rm -rf $1/.git
 
   if [ $2 ] ; then
     sed -i -e "/^export $2=.*/d" ~/.bashrc
@@ -29,8 +29,8 @@ case $input in
     init navy-apps NAVY_HOME
     source ~/.bashrc
 
-    git add -A
-    git commit -am "ics2017 initialized"$'\n\n'"$log"
+#    git add -A
+#    git commit -am "ics2017 initialized"$'\n\n'"$log"
 
     echo "Initialization finishes!"
     ;;
