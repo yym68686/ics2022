8ecb414e9479f9289e9635858335637b83c79e6a
161530115-Jin Hang <jinhang@nuaa.edu.cn>
2019-05-27 18:27:45 +0800

    update syscall.c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -36,24 +36,10 @@ static inline uintptr_t sys_brk(uintptr_t new_brk) {
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
-  a[1] = SYSCALL_ARG2(r);
-  a[2] = SYSCALL_ARG3(r);
-  a[3] = SYSCALL_ARG4(r);
-  int ret = 0;
 
   switch (a[0]) {
-    case SYS_none: ret = 1; break;
-    case SYS_exit: _halt(a[1]); break;
-    case SYS_brk: ret = sys_brk(a[1]); break;
-    case SYS_open: ret = sys_open(a[1], a[2], a[3]); break;
-    case SYS_write: ret = sys_write(a[1], a[2], a[3]); break;
-    case SYS_read: ret = sys_read(a[1], a[2], a[3]); break;
-    case SYS_lseek: ret = sys_lseek(a[1], a[2], a[3]); break;
-    case SYS_close: ret = sys_close(a[1]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
-
-  SYSCALL_ARG1(r) = ret;
 
   return NULL;
 }
