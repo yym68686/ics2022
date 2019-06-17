ca5713c8269c6d7e14fed5990bdb3028878fafa3
161530115-Jin Hang <jinhang@nuaa.edu.cn>
2019-06-17 15:09:55 +0800

    update pte.c

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -81,14 +81,5 @@ void _unmap(_Protect *p, void *va) {
 }
 
 _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
-  ustack.end -= 4 * sizeof(int);  // 4 = retaddr + argc + argv + envp
-  int *p0 = ustack.end;
-  p0[0] = p0[1] = p0[2] = p0[3] = 0;
-
-  _RegSet *r = (_RegSet*)ustack.end - 1;
-
-  r->cs = 0x8;
-  r->eip = (uintptr_t)entry;
-  r->eflags = 0x2 | FL_IF;
-  return r;
+  return NULL;
 }
