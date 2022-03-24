6030cb3bb50c70ee3d62b3f5c608cb8356e28294
tracer-ics2017 <tracer@njuics.org>
2022-03-24 10:34:20 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  10:34:20 up  8:12,  2 users,  load average: 0.14, 0.04, 0.01 32c9b6cfb0a96f437f86898dd92c703813bc674d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,6 +37,12 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+static int cmd_si(char *args){
+    char *arg = strtok(NULL, " ");
+	puts(arg);
+// 	cpu_exec();
+	return 0;
+}
 
 static struct {
   char *name;
@@ -46,6 +52,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "step in", cmd_si },
 
   /* TODO: Add more commands */
 
