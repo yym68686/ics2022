ef0e19344e046c880f0b060c370fb8b0795cc368
tracer-ics2017 <tracer@njuics.org>
2022-04-02 17:49:51 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  17:49:51 up  8:24,  3 users,  load average: 0.18, 0.07, 0.01 21d2671e6469b9716e17eacf6960f2c9ab11ea7f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -132,10 +132,12 @@ static bool make_token(char *e) {
         }
 		if (flag == 0){
 			strncpy(tokens[nr_token++].str, e + position, substr_len);
-			for (int j = 0; j < nr_token; j++){
-				printf("|%s| ", tokens[j].str);
-			}
-			puts("");
+			printf("|%s| ", tokens[nr_token - 1].str);
+
+// 			for (int j = 0; j < nr_token; j++){
+// 				printf("|%s| ", tokens[j].str);
+// 			}
+// 			puts("");
 
 		}
         break;
