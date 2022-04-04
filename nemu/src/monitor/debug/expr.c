0adaebf248c79cb7be126b654b58f826b27f66ef
tracer-ics2017 <tracer@njuics.org>
2022-04-04 18:14:41 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  18:14:41 up 44 min,  2 users,  load average: 0.06, 0.02, 0.00 e13a3df336c1ede3531643364fdb28201686ca08

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -219,7 +219,6 @@ uint32_t eval(int p, int q) {
     }
     else {
 		int op = find_dominated_op(p, q);
-		printf("*%d\n", op);
         int val1 = eval(p, op - 1);
         int val2 = eval(op + 1, q);
         switch (tokens[op].type) {
