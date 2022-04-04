30ff9955000667d9f5333238eeee33a5442ee93d
tracer-ics2017 <tracer@njuics.org>
2022-04-04 14:59:10 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  14:59:10 up 1 day, 2 min,  2 users,  load average: 0.01, 0.03, 0.00 7dc2e13b6debc2f8fe3562b0cbc7c3c8ea5fa20d

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -97,7 +97,7 @@ static int cmd_x(char *args){
 }
 static int cmd_p(char *args){
 	bool success = true;
-	int result = expr(args, &success);
+	uint32_t result = expr(args, &success);
 	if (success == true && result != 0x3f3f3f3f)
 		printf("%d\n", result);
 	return 0;
