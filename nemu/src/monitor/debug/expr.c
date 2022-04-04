83b4d2686db952a5cb4d183506d2ee2c552acf34
tracer-ics2017 <tracer@njuics.org>
2022-04-04 15:12:51 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  15:12:51 up 1 day, 16 min,  2 users,  load average: 0.00, 0.03, 0.00 6ad3dba71887660620c84ea12f8f9c743232d2ae

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -200,8 +200,8 @@ uint32_t eval(int p, int q) {
 				len += strlen(tokens[i].str);
 		}
 		printf("\n%*c\n", len + 21, '^');
+// 		*success = false;
 		return 0x3f3f3f3f;
-// 		assert(0);
         /* Bad expression */
     }
     else if (p == q) {
