106524016ccf7beedcbe40a4b071fc9bb70e2b90
tracer-ics2017 <tracer@njuics.org>
2022-04-09 20:24:39 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  20:24:39 up  1:24,  2 users,  load average: 0.02, 0.03, 0.00 68d2cc7783cf9ff59ec0de34136144305944c599

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -97,7 +97,7 @@ bool delete_watchpoint(int NO){
 	if (!head) return false;
 	WP* tmp = head;
 	while (tmp){
-		printf("delete_watchpoint:%d\n", tmp->NO);
+		printf("delete_watchpoint:%d free_->NO:%d\n", tmp->NO, free_->NO);
 		if (tmp->NO == NO){
 			free_wp(tmp);
 			return true;
