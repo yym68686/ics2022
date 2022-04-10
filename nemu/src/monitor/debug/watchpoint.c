86ce6162f77c4bf538a4b7a36fa285f8afc2c0b4
tracer-ics2017 <tracer@njuics.org>
2022-04-10 17:08:17 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  17:08:17 up  8:15,  2 users,  load average: 0.02, 0.03, 0.00 5bf9b9393258967f55bb4e92685ba9b79422b6e

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -192,7 +192,7 @@ WP* scan_watchpoint(void){
 		if (success == true && result != -1162167624) cur->new_val = result;
 		else return NULL;
 		if (cur->old_val != cur->new_val){
-			printf("Hit watchpoint %d at address 0x%d", cur->NO);
+			printf("Hit watchpoint %d at address 0x%d", cur->NO, cur->NO);
 			printf("expr      = %s\n", cur->expr);
 			printf("old value = %d\nnew value = %d\n", cur->old_val, cur->new_val);
 			puts("program paused");
