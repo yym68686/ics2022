8ca9576a75e61f417c2546815a1bd87adb1aa605
tracer-ics2017 <tracer@njuics.org>
2022-04-27 00:10:21 +0800

    >  compile 161940233 yanyuming Linux PA 5.10.0-12-686-pae #1 SMP Debian 5.10.103-1 (2022-03-07) i686 GNU/Linux  00:10:21 up 12:48,  2 users,  load average: 0.04, 0.01, 0.00 143ea4bc42ce36bc73e195217fb8e3b8bfc80da0

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -6,9 +6,9 @@ make_EHelper(mov) {
 }
 
 make_EHelper(push) {
-  t0 = id_dest->val;
-  rtl_push(&t0);
-//   rtl_push(&(id_dest->val));
+//   t0 = id_dest->val;
+//   rtl_push(&t0);
+  rtl_push(&(id_dest->val));
 
   print_asm_template1(push);
 }
