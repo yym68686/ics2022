114fbcae01655b392af3e3b434a1f33fdee5018d
tracer-ics2017 <tracer@njuics.org>
2022-05-08 23:41:49 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  23:41:49 up 15:58,  2 users,  load average: 0.02, 0.01, 0.00 94b3a16d5aa261815b660b014a1c21ac3824b66c

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -68,9 +68,9 @@ rtl_sltu(&t0, &id_dest->val, &t1);
 
 rtl_set_CF(&t0);
 
-rtl_xor(&t0, &id_dest->val, &id_src->val);
+rtl_xor(&t2, &id_dest->val, &id_src->val);
 rtl_xor(&t1, &id_dest->val, &t1);
-rtl_and(&t0, &t0, &t1);
+rtl_and(&t0, &t2, &t1);
 rtl_msb(&t0, &t0, id_dest->width);
 rtl_set_OF(&t0);
 
