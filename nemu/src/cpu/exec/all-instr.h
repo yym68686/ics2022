518991482e3fa2b92d31e1defd3d6d673872a267
161940233-yanyuming <yym68686@outlook.com>
2022-05-09 02:55:53 +0800

    完成 inc 指令

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -26,3 +26,4 @@ make_EHelper(test);
 make_EHelper(setcc);
 make_EHelper(movzx);
 make_EHelper(leave);
+make_EHelper(inc);
--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -62,9 +62,17 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-  TODO();
+	rtl_addi(&t3, &id_dest->val, 1);
+	operand_write(id_dest, &t3);
 
-  print_asm_template1(inc);
+	// 更新ZF,SF标志位
+	rtl_update_ZFSF(&t3, id_dest->width); // rtl_update_ZFSF函数内部临时变量是t0，所以不能用t0传参，否则更新SF会出错，因为更新ZF时，t0会变
+
+	// 更新OF标志位
+	rtl_xor(&t2, &id_dest->val, &t3);
+	rtl_msb(&t2, &t2, id_dest->width);
+	rtl_set_OF(&t2);
+	print_asm_template1(inc);
 }
 
 make_EHelper(dec) {
