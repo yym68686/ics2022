eebad7c936cd241b83b4665e903a12fed9e29f16
tracer-ics2017 <tracer@njuics.org>
2022-05-08 16:34:52 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  16:34:52 up 11:54,  3 users,  load average: 0.00, 0.00, 0.00 26501300c49443c15145e76dcb62cf9327363d70

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -5,8 +5,8 @@ make_EHelper(add) {
   operand_write(id_dest, &t1); //写入目的操作数
   rtl_update_ZFSF(&t1, id_dest->width); //更新ZFSF位
   t1 = 0;
-  rtl_set_OF(&t0); //设置OF位为0
-  rtl_set_CF(&t0); //设置CF位为0
+  rtl_set_OF(&t1); //设置OF位为0
+  rtl_set_CF(&t1); //设置CF位为0
 
   print_asm_template2(add);
 }
