eec0fd61f276da25d12f01d6fc3f61ee00d23271
tracer-ics2017 <tracer@njuics.org>
2022-05-13 17:13:45 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  17:13:45 up 1 day,  2:29,  3 users,  load average: 0.08, 0.03, 0.01 671d678415aa4ad7afdd49a9bf499a9fa6be20f

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -19,15 +19,15 @@ make_EHelper(pop) {
 }
 
 make_EHelper(pusha) {
-  rtlreg_t temp = cpu.esp;
-  rtl_push(&cpu.eax);
-  rtl_push(&cpu.ecx);
-  rtl_push(&cpu.edx);
-  rtl_push(&cpu.ebx);
-  rtl_push(&temp);
-  rtl_push(&cpu.ebp);
-  rtl_push(&cpu.esi);
-  rtl_push(&cpu.edi);
+//   rtlreg_t temp = cpu.esp;
+//   rtl_push(&cpu.eax);
+//   rtl_push(&cpu.ecx);
+//   rtl_push(&cpu.edx);
+//   rtl_push(&cpu.ebx);
+//   rtl_push(&temp);
+//   rtl_push(&cpu.ebp);
+//   rtl_push(&cpu.esi);
+//   rtl_push(&cpu.edi);
   print_asm("pusha");
 }
 
