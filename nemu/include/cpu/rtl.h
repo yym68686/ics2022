2df2a334db6476d95a0ed5409368cc440b57b528
tracer-ics2017 <tracer@njuics.org>
2022-05-14 18:05:33 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  18:05:33 up 1 day, 11:30,  4 users,  load average: 0.29, 0.15, 0.10 b2da21da3785561d67af4729253f3d8939d6717b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -200,7 +200,7 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   t0 = *result >> (width * 8 - 1) & 0x1;
-  printf("updateSF: 0x%08x width: %d\n", t0, width);
+  printf("result:0x%08x updateSF: 0x%08x width: %d\n", *result, t0, width);
   rtl_set_SF(&t0);
 }
 
