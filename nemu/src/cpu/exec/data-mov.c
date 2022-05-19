c038133cffee8e3474a23039cb98864706fc5a72
tracer-ics2017 <tracer@njuics.org>
2022-05-19 10:31:49 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  10:31:49 up  6:26,  3 users,  load average: 0.22, 0.16, 0.25 3a422e083041f35be014a769e45356ff7e261d9a

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -55,12 +55,14 @@ make_EHelper(cwtl) {
   if (decoding.is_operand_size_16) {
       rtl_lr_b(&t0, R_AL); // 取出 AL 放到t0里
   	  rtl_sext(&t0, &t0, 1);
-	  reg_w(R_AX) = t0;
+// 	  reg_w(R_AX) = t0;
+      rtl_sr_w(R_AX, &t0);
   }
   else {
       rtl_lr_w(&t0, R_AX); // 取出 AX 放到t0里
   	  rtl_sext(&t0, &t0, 2);
-	  reg_w(R_EAX) = t0;
+// 	  reg_w(R_EAX) = t0;
+      rtl_sr_l(R_EAX, &t0);
   }
 
   print_asm(decoding.is_operand_size_16 ? "cbtw" : "cwtl");
