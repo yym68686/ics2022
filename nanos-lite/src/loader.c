d5db5fbab9235f5a75059bc7de405b1183b45c25
tracer-ics2017 <tracer@njuics.org>
2022-05-27 12:07:03 +0800

    >  run 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  12:07:03 up 2 days,  7:55,  3 users,  load average: 0.49, 0.16, 0.05 d41855499810f761af4a9ca78fef8c692d1f7297

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,10 @@
 #include "common.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
+void ramdisk_read(void *, uint32_t, uint32_t);
+size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
   return (uintptr_t)DEFAULT_ENTRY;
 }
