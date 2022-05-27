90fa745b02577a8e97e2739633be0506fb8fee33
161940233-yanyuming <yym68686@outlook.com>
2022-05-27 19:08:58 +0800

    实现 lidt 指令

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -4,8 +4,11 @@ void diff_test_skip_qemu();
 void diff_test_skip_nemu();
 
 make_EHelper(lidt) {
-  TODO();
-
+  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);       //limit 16位
+  if (decoding.is_operand_size_16)
+      cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);//base 24位
+  else
+      cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);//base 32位
   print_asm_template1(lidt);
 }
 
