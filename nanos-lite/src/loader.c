45e8046273da01c3fa7e1aac5abafcf498a0fc80
161940233-yanyuming <yym68686@outlook.com>
2022-06-12 15:50:36 +0800

    修改 loader()

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,13 +1,24 @@
 #include "common.h"
 #include "fs.h"
-#define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void *)0x8048000)
 void ramdisk_read(void *, uint32_t, uint32_t);
 size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  int fd = fs_open(filename, 0, 0);
-  size_t f_size = fs_filesz(fd);
-  fs_read(fd, DEFAULT_ENTRY, f_size);
-  fs_close(fd);
-  return (uintptr_t)DEFAULT_ENTRY;
+    int fd = fs_open(filename, 0, 0);
+    size_t f_size = fs_filesz(fd);
+    uint32_t vaddr, paddr;
+	uint32_t endaddress = (uint32_t)DEFAULT_ENTRY + f_size;
+
+    //进行分页映射并加载到内存位置0x8048000附近
+    for (vaddr = (uint32_t)DEFAULT_ENTRY; vaddr < endaddress; vaddr += PGSIZE) {
+        paddr = new_page();
+        Log("Map va to pa: 0x%08x to 0x%08x", vaddr, paddr);
+        _map(as, (void *)vaddr, (void *)paddr);
+        fs_read(fd, paddr, (endaddress - vaddr) < PGSIZE ? endaddress : PGSIZE);
+    }
+
+    fs_close(fd);
+
+    return (uintptr_t)DEFAULT_ENTRY;
 }
