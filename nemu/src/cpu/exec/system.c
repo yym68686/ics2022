8716a6652dce124d97aee4bd285c5afb21545fe7
161940233-yanyuming <yym68686@outlook.com>
2022-06-12 15:10:19 +0800

    修改访存函数 增加mov_r2cr mov_cr2r

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -13,19 +13,38 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
+    switch (id_dest->reg) {
+        case 0:
+            cpu.cr0.val = id_src->val;
+            break;
+        case 3:
+            cpu.cr3.val = id_src->val;
+            break;
+        default:
+            assert(0);
+    }
 
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
+    print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
+    switch (id_src->reg) {
+        case 0:
+            t0 = cpu.cr0.val;
+            break;
+        case 3:
+            t0 = cpu.cr3.val;
+            break;
+        default:
+            assert(0);
+    }
+    operand_write(id_dest, &t0);
 
-  print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
+    print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
-#ifdef DIFF_TEST
-  diff_test_skip_qemu();
-#endif
+    #ifdef DIFF_TEST
+    diff_test_skip_qemu();
+    #endif
 }
 
 void raise_intr(uint8_t NO, vaddr_t ret_addr);
