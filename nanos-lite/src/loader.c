b89dc85496d5c235dd16c9d845c80bf5146345ad
161940233-yanyuming <yym68686@outlook.com>
2022-06-12 15:56:34 +0800

    修改 loader() 增加void *new_page(size_t nr_page);

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,7 @@
 #define DEFAULT_ENTRY ((void *)0x8048000)
 void ramdisk_read(void *, uint32_t, uint32_t);
 size_t get_ramdisk_size();
+void *new_page(size_t);
 
 uintptr_t loader(_Protect *as, const char *filename) {
     int fd = fs_open(filename, 0, 0);
@@ -15,7 +16,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
         paddr = new_page();
         Log("Map va to pa: 0x%08x to 0x%08x", vaddr, paddr);
         _map(as, (void *)vaddr, (void *)paddr);
-        fs_read(fd, paddr, (endaddress - vaddr) < PGSIZE ? endaddress : PGSIZE);
+        fs_read(fd, (void *)paddr, (endaddress - vaddr) < PGSIZE ? endaddress : PGSIZE);
     }
 
     fs_close(fd);
