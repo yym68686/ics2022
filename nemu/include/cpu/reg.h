c5e47788ec6e07773f165bbaf4f845516ea19fd6
161940233-yanyuming <yym68686@outlook.com>
2022-06-12 14:59:01 +0800

    修改访存函数 #include memory/mmu.h

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,6 +2,7 @@
 #define __REG_H__
 
 #include "common.h"
+#include "memory/mmu.h"
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
