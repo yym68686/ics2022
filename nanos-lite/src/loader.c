b03ad3547f6b550687728c2955d986b1fd2383ae
tracer-ics2017 <tracer@njuics.org>
2022-06-12 17:09:57 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  17:09:57 up  4:23,  3 users,  load average: 0.28, 0.07, 0.02 659d2aaa86f6fd51e9ed5727db48db426211877

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,18 +7,15 @@ void *new_page();
 
 uintptr_t loader(_Protect *as, const char *filename) {
     int fd = fs_open(filename, 0, 0);
-    size_t f_size = fs_filesz(fd);
-    uint32_t vaddr, paddr;
-	uint32_t endaddress = (uint32_t)DEFAULT_ENTRY + f_size;
-
-    //进行分页映射并加载到内存位置0x8048000附近
-    for (vaddr = (uint32_t)DEFAULT_ENTRY; vaddr < endaddress; vaddr += PGSIZE) {
-        paddr = (uint32_t)new_page();
-        Log("Map va to pa: 0x%08x to 0x%08x", vaddr, paddr);
-        _map(as, (void *)vaddr, (void *)paddr);
-        fs_read(fd, (void *)paddr, (endaddress - vaddr) < PGSIZE ? endaddress : PGSIZE);
+    int filesize = fs_filesz(fd);
+    void *pa;
+    void *va = DEFAULT_ENTRY;
+    while(filesize > 0) {
+        pa = new_page();
+        _map(as, va, pa);
+        va += PGSIZE;
+        fs_read(fd, pa, PGSIZE);
+        filesize -= PGSIZE;
     }
-    fs_close(fd);
-
     return (uintptr_t)DEFAULT_ENTRY;
 }
