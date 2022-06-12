f4b5a273edbd5642686e777284d8212cbb695c23
161940233-yanyuming <yym68686@outlook.com>
2022-06-12 14:13:50 +0800

    修改访存函数 增加page_translate

--- nemu/include/memory/memory.h
+++ nemu/include/memory/memory.h
@@ -12,7 +12,9 @@ extern uint8_t pmem[];
 
 uint32_t vaddr_read(vaddr_t, int);
 uint32_t paddr_read(paddr_t, int);
+paddr_t page_translate(vaddr_t vaddr, bool flag);
 void vaddr_write(vaddr_t, int, uint32_t);
 void paddr_write(paddr_t, int, uint32_t);
+
 
 #endif
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -28,12 +28,17 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
     if(cpu.cr0.paging) {
-        //跨页访存 0xfff = 111111111111 = 2^12 - 1 = 4KB - 1
-        if ((addr & 0xfff) + len - 1 > 0xfff) {
-            uint32_t val = 0;
-            for (int t = 0; i < len; i++)
-                val += (1 << (8 * i)) * (uint8_t)paddr_read(page_translate(addr + i, false), 1);
-            return val;
+        //跨页访存 0x1000 = 1000000000000 = 2^12 = 4KB
+        if ((addr & 0xfff) + len > 0x1000) {
+			union {
+			  uint8_t bytes[4];
+			  uint32_t dword;
+			} data = {0};
+			for (int i = 0; i < len; i++) {
+			  paddr = page_translate(addr + i, false);
+			  data.bytes[i] = (uint8_t)paddr_read(paddr, 1);
+			}
+			return data.dword;
         }
         else {
             paddr_t paddr = page_translate(addr, false);
@@ -44,11 +49,44 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
         return paddr_read(addr, len);
 }
 
+paddr_t page_translate(vaddr_t vaddr, bool flag) {
+    PDE page_dir_item;
+    PTE page_table_item;
+    //页目录项的地址 = 页目录表基地址 + (页目录表索引) * 4
+    paddr_t page_dir_item_addr = (cpu.cr3.page_directory_base << 12) + ((vaddr >> 22) & 0x3ff) * 4;
+    //读取页目录项
+    page_dir_item.val = paddr_read(page_dir_item_addr, 4);
+    //验证present位
+    assert(page_dir_item.present);
+    //根据讲义，accessed为1，若为1表示该页被CPU访问过，由CPU置1，由操作系统清0
+    page_dir_item.accessed = 1;
+    //写回到页目录项所在地址
+    paddr_write(page_dir_item_addr, 4, page_dir_item.val);
+    //页表项的地址 = 页目录表项对应的页表基址 + 页表索引 * 4
+    paddr_t page_table_item_addr = (page_dir_item.page_frame << 12) + ((vaddr >> 12) & 0x3ff) * 4;
+    //读取页表项
+    page_table_item.val = paddr_read(page_table_item_addr, 4);
+    //验证present位
+    assert(page_table_item.present);
+    page_table_item.accessed = 1;
+    //如果是写操作，脏位设为1，当CPU对一个页执行写操作时，设置对应页表项的D位为1，此项仅针对页表项有效，并不会修改页目录表项的D位
+    if (flag) page_table_item.dirty = 1;
+    //写回到页表项所在地址
+    paddr_write(page_table_item_addr, 4, page_table_item.val);
+    paddr_t paddr = (page_table_item.page_frame << 12) + (vaddr & 0xfff);
+    //Log("vaddr: %#10x, paddr: %#10x", vaddr, paddr);
+    return paddr;
+}
+
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
     if(cpu.cr0.paging) {
-        //跨页访存.
-        if ((addr & 0xfff) + len - 1 > 0xfff)
-            assert(0);
+        //跨页访存
+        if ((addr & 0xfff) + len > 0x1000)
+		    for (int i = 0; i < len; i++) {
+			  paddr = page_translate(addr + i, true);
+			  paddr_write(paddr, 1, data);
+			  data >>= 8;
+			}
         else {
             paddr_t paddr = page_translate(addr, true);
             return paddr_write(paddr, len, data);
