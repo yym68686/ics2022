8e326126cf24963bc21f2dcc4bb9b498e8923216
tracer-ics2017 <tracer@njuics.org>
2022-06-12 16:44:01 +0800

    >  compile 161940233 yanyuming Linux PA 4.19.0-20-686-pae #1 SMP Debian 4.19.235-1 (2022-03-17) i686 GNU/Linux  16:44:01 up  3:57,  2 users,  load average: 0.00, 0.00, 0.00 d6d7064736b2f514ce19b9567daba6552563600

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,7 +3,7 @@
 #define DEFAULT_ENTRY ((void *)0x8048000)
 void ramdisk_read(void *, uint32_t, uint32_t);
 size_t get_ramdisk_size();
-void *new_page(size_t);
+void *new_page();
 
 uintptr_t loader(_Protect *as, const char *filename) {
     int fd = fs_open(filename, 0, 0);
@@ -13,7 +13,7 @@ uintptr_t loader(_Protect *as, const char *filename) {
 
     //进行分页映射并加载到内存位置0x8048000附近
     for (vaddr = (uint32_t)DEFAULT_ENTRY; vaddr < endaddress; vaddr += PGSIZE) {
-        paddr = new_page();
+        paddr = (uint32_t)new_page();
         Log("Map va to pa: 0x%08x to 0x%08x", vaddr, paddr);
         _map(as, (void *)vaddr, (void *)paddr);
         fs_read(fd, (void *)paddr, (endaddress - vaddr) < PGSIZE ? endaddress : PGSIZE);
